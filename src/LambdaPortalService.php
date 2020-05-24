c38ec4cf32098ef6353cd87577cd6188bb8919e7
Tlapi <jan@ecomail.cz>
2020-05-24 23:48:47 +0200

     add portal service

---
+++ src/LambdaPortalService.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types = 1);
+
+namespace Tlapi\LambdaPortal;
+
+use Aws\Lambda\LambdaClient;
+use Aws\Result;
+use Illuminate\Queue\Jobs\SqsJob;
+use Illuminate\Queue\Jobs\SyncJob;
+
+class LambdaPortalService
+{
+
+    /**
+     * @var \Aws\Lambda\LambdaClient
+     */
+    protected $client;
+
+    public function __construct()
+    {
+        $this->client = new LambdaClient(config('queue.connections.lambda-sync'));
+    }
+
+    public function sendJobToLambda(string $payload): Result
+    {
+        return $this->client->invoke([
+            'FunctionName' => 'ecm-lambda-sqs-worker-dev-worker',
+            'Payload' => $payload,
+        ]);
+    }
+
+    public function processJobByLambda($container, array $payload)
+    {
+        $queueJob = new SyncJob($container, json_encode($payload), 'lambda-sync', 'lambda-sync');
+        $queueJob->fire();
+    }
+
+}
